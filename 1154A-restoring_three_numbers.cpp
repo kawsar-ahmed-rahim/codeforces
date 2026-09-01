9309e3af607659a35be68da65d49c9ddc81e3c3b
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-01 21:35:35 +0600

    till now

---
+++ 1154A-restoring_three_numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int x[4];
+
+    for (int i = 0; i < 4; i++) {
+        cin >> x[i];
+    }
+
+    sort(x, x + 4);
+
+    cout << x[3] - x[0] << " "
+         << x[3] - x[1] << " "
+         << x[3] - x[2] << endl;
+
+    return 0;
+}
