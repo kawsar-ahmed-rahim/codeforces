029d26f0d4c805e6a0b232d6fab7144bc78f688f
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-05 01:22:48 +0600

    2123A

---
+++ 2123A-blackboard_game.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) {
+        cin >> n;
+
+        if(n % 4 == 0) {
+            cout << "Bob" << endl;
+        } else {
+            cout << "Alice" << endl;
+        }
+    }
+
+    return 0;
+}
