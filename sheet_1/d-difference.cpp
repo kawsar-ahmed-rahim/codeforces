4373f8ad35ad67bc0089c1724f1c0f1e327c677a
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-10 09:34:37 +0600

    till now

--- sheet_1/d-difference.cpp
+++ sheet_1/d-difference.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    int a, b,c,d;
+    long long a, b,c,d;
     cin >> a >> b >> c >> d;
 
-    int ans = ((a*b)-(c*d));
+    long long ans = ((a*b)-(c*d));
     cout << "Difference = " << ans << endl;
     return 0;
 }
