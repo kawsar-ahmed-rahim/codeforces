732a44e64071be21f1da8eea19e8f2aa4aab4e67
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-12 13:53:21 +0600

    till now

---
+++ sheet_1/L-The_Brothers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+
+int main() {
+    string a1, a2, b1, b2;
+    cin >> a1 >> a2;
+     cin >> b1 >> b2;
+
+
+    if(a2==b2){
+        cout << "ARE Brothers" << endl;
+    }else {
+         cout << "NOT" << endl;
+    }
+}
