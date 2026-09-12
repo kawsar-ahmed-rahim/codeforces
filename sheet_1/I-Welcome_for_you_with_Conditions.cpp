94effa3aecaec9a4f19e499c363b6390bb7852c3
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-12 12:37:06 +0600

    I

---
+++ sheet_1/I-Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if(b>a){
+        cout << "No" << endl;
+    }else {
+        cout << "Yes" << endl;
+    }
+
+    return 0;
+}
