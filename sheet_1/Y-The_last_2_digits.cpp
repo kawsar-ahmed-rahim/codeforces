a2b2e7f79eff0ef31cfe8ef1ce84050c84b87f1b
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-14 19:15:40 +0600

    till now

--- sheet_1/Y-The_last_2_digits.cpp
+++ sheet_1/Y-The_last_2_digits.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-#include<iomanip>
 using namespace std;
 
 int main() {
-    int a,b,c,d;
+    long long a, b, c, d;
     cin >> a >> b >> c >> d;
 
-    int ans = a*b*c*d;
+    long long ans = (a % 100) * (b % 100) * (c % 100) * (d % 100);
 
-    cout << (ans%100) << endl;
-    
+    cout << ans % 100 << endl;
+
     return 0;
 }
