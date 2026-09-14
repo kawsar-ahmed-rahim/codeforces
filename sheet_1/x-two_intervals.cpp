34e028f683059e26adcde1ee70529bc65c11efc5
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-14 19:05:06 +0600

    x

---
+++ sheet_1/x-two_intervals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int left = max(a, c);
+    int right = min(b, d);
+
+    if (left <= right) {
+        cout << left << " " << right << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
+    return 0;
+}
