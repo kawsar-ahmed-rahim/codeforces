e6b5bda3c92d265fed60c24752b56d52ce078527
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-17 20:22:04 +0600

    q

---
+++ sheet_2/q-digits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n == 0) {
+            cout << 0;
+        }
+
+        while (n > 0) {
+            cout << n % 10 << " ";
+            n /= 10;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
